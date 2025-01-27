5d1f94243904a234b7c6b3e1cec94e5dbd0bb7d6
goyourpunch <berezinskiy00@mail.ru>
2025-01-27 18:34:45 +0100

    test?

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Yo brooo")
+print("Yo brooo") #выводит текст в консоль
+
+age = 24 #целое число (int)
+height = 1.75 #дробное число (float)
+name = "Mike" #строка (str)
+is_student = True #булевое значение (bool)
+
+print("my name is",name,age,"years")
+print(name, "is", age, "years")
+
+age = 66
+name = "Scott"
+car = "BMW"
+address = "New York"
+dick = 16.7
+
+print("hello, my name is", name, "i am", age, "years old,","driving car", car, ", i live", address, ",", "my dick", dick, "bye!")
---
+++ main1.py
@@ -0,0 +1,12 @@
+name = input("What is your name?") #позволяет программе запрашивать информацию у пользователя
+age = int(input("What is your age?")) # Преобразуем ввод в целое число
+
+# print("Yooo,", name + "!", "10 years ago you were", age - 10)
+
+height = float(input("какой у тебя рост?"))
+weight = int(input("какой у тебя вес?"))
+car = input("какую ты хочешь машину?")
+address = input("в каком ты городе хочешь жить?")
+animal = input("какое животное тебе нравиться?")
+
+print("Suuup", name, "!", "твой ментальный возраст", age + 10, "твой рост мог бы быть ", height * 1.5, "метра)))", "почему ты не весишь", weight * 2, "?", "зачем тебе", car, "?", "на самом деле я знаю зачем!) чтобы поехать в", address, "и прехватить с собой 6", animal, "!", "Bye brooooooo!!")
